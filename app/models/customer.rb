5a8e6eef54a17d35029458a20844160d1e9bb825
BobaFett1 <michelehmen@hotmail.de>
2016-02-17 19:10:34 +0100

    Fehlerbehebung und Anpassung von production_order_attachments

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ActiveRecord::Base
   has_many :production_orders, inverse_of: :customer
 
+  validates :name1, presence: true
+
   def display_name
     "#{code} - #{name1}"
   end
